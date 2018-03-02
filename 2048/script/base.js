7df0568ff95b0b581cfdea904cf9356a343bdd43
liuwx <36917151+lwxsanshui@users.noreply.github.com>
2018-03-02 20:16:32 +0800

    Add files via upload

---
+++ 2048/script/base.js
@@ -0,0 +1,300 @@
+/**
+ * Created by wjj on 2018/3/2.
+ */
+
+var board = new Array();
+var score = 0;
+$(document).ready(function(e){
+    newgame();
+});
+//初始化
+function newgame(){
+    init();
+    generateNumber();
+    generateNumber();
+    updateBoardView();
+}
+function init(){
+    for(var i=0 ; i<4 ; i++){
+        for(var j=0 ; j<4 ; j++){
+            var gridCell = $("#grid-"+i+"-"+j);
+            gridCell.css("top",getPosTop(i,j));
+            gridCell.css("left",getPosLeft(i,j))
+        }
+    }
+    for(var i=0 ; i<4 ; i++){
+        board[i] = new Array();
+        for(var j=0 ; j<4 ; j++){
+            board[i][j] = 0 ;
+        }
+    }
+    updateBoardView();
+}
+function updateBoardView(){
+    $(".number-cell").remove();
+    score = 0;
+    for(var i=0 ; i<4 ;i++){
+        for(var j=0 ; j<4 ;j++){
+            $("#grid-container").append('<div class="number-cell" id="number-cell-'+i+'-'+j+'"></div>');
+            var theNumberCell = $('#number-cell-'+i+'-'+j);
+            if(board[i][j]==0){
+                theNumberCell.css('width','0px');
+                theNumberCell.css('height','0px');
+                theNumberCell.css('top',getPosTop(i,j));
+                theNumberCell.css('left',getPosLeft(i,j));
+            }else{
+                score+=board[i][j];
+                theNumberCell.css('width','100px');
+                theNumberCell.css('height','100px');
+                theNumberCell.css('top',getPosTop(i,j));
+                theNumberCell.css('left',getPosLeft(i,j));
+                //设定背景色和数字颜色
+                theNumberCell.css('background-color',getNumberBackgroundColor(board[i][j]));
+                theNumberCell.css('color',getNumberColor(board[i][j]));
+                theNumberCell.text(board[i][j]);
+                if(board[i][j]>100){
+                    theNumberCell.css('fontSize','43px');
+                }
+                if(board[i][j]>1000){
+                    theNumberCell.css('fontSize','37px');
+                }
+            }
+        }
+    }
+    $("#score-2").text(score);
+
+}
+function generateNumber(){
+    if (nospace(board))return false;
+    //随机取一个位置
+    var randx = parseInt(Math.floor(Math.random()*4));
+    var randy = parseInt(Math.floor(Math.random()*4));
+    while(board[randx][randy]!=0){
+        //优化
+        var randx = parseInt(Math.floor(Math.random()*4));
+        var randy = parseInt(Math.floor(Math.random()*4));
+    }
+    //随机取一个数字
+    var randNumber = Math.random()<0.8?2:4;
+    board[randx][randy] = randNumber;
+    showNumberWithAnimation(randx,randy,randNumber);
+    return true;
+}
+$("#newGameButton").click(newgame);
+$(document).keydown(function(event){
+   switch (event.keyCode){
+       case 37://left
+           if(moveLeft()){
+               generateNumber();
+               isGameOver();
+           }
+           break;
+       case 38://up
+           if(moveUp()){
+               generateNumber();
+               isGameOver();
+           }
+           break;
+       case 39://right
+           if(moveRight()){
+               generateNumber();
+               isGameOver();
+           }
+           break;
+       case 40://down
+           if(moveDown()){
+               generateNumber();
+               isGameOver();
+           }
+           break;
+   }
+});
+function isGameOver(){
+    if(nospace(board)&&nomove(board))
+        alert("gameover");
+}
+function moveLeft(){
+    if(!canMoveLeft(board))return false;
+    console.log("可以移动，那就开始了");
+    //移动过程
+    var hasMove = false;//标记当前行是否移动过；
+    for(var i=0 ; i<4 ; i++){
+        for(var j=1 ; j<4 ; j++){
+            if(board[i][j]!=0){
+                //判断它左侧的元素,
+                // 这里的逻辑是只需要获取其左侧最近的不等于0的值，
+                // 相等则合并，不相等则移动至该位置+1
+                //根据hasMove可以避免多次合并
+                for(var k=j-1 ; k>=0 ; k--){
+                    console.log(i,j,k);
+                    if(board[i][k]!=0){
+                        if(board[i][k]!=board[i][j]||hasMove ==true){
+
+                            if(k+1<j){
+                                showMoveAnimation(i,j,i,k+1);
+                                board[i][k+1] =board[i][j];
+                                board[i][j] = 0 ;
+
+                            }
+                            k=-1;
+                            hasMove = false;
+                        }else{
+                            showMoveAnimation(i,j,i,k);
+                            board[i][k] += board[i][j];
+                            board[i][j] = 0;
+                            k=-1;
+                            hasMove = true;
+                        }
+                    }else if(k == 0 ){
+                        showMoveAnimation(i,j,i,k);
+                        board[i][k] =board[i][j];
+                        board[i][j] = 0 ;
+                    }
+                }
+            }
+        }
+        hasMove = false;
+
+    }
+    setTimeout("updateBoardView()",200);
+    return true;
+}
+function moveRight(){
+    if(!canMoveRight(board))return false;
+    console.log("可以移动，那就开始了");
+    //移动过程
+    var hasMove = false;//标记当前行是否移动过；
+    for(var i=0 ; i<4 ; i++){
+        for(var j=2 ; j>=0 ; j--){
+            if(board[i][j]!=0){
+                //判断它左侧的元素,
+                // 这里的逻辑是只需要获取其左侧最近的不等于0的值，
+                // 相等则合并，不相等则移动至该位置+1
+                //根据hasMove可以避免多次合并
+                for(var k=j+1 ; k<4 ; k++){
+                    console.log(i,j,k);
+                    if(board[i][k]!=0){
+                        if(board[i][k]!=board[i][j]||hasMove ==true){
+
+                            if(k-1>j){
+                                showMoveAnimation(i,j,i,k-1);
+                                board[i][k-1] =board[i][j];
+                                board[i][j] = 0 ;
+
+                            }
+                            k=4;
+                            hasMove = false;
+                        }else{
+                            showMoveAnimation(i,j,i,k);
+                            board[i][k] += board[i][j];
+                            board[i][j] = 0;
+                            k=4;
+                            hasMove = true;
+                        }
+                    }else if(k == 3 ){
+                        showMoveAnimation(i,j,i,k);
+                        board[i][k] =board[i][j];
+                        board[i][j] = 0 ;
+                    }
+                }
+            }
+        }
+        hasMove = false;
+
+    }
+    setTimeout("updateBoardView()",200);
+    return true;
+}
+function moveUp(){
+    if(!canMoveUp(board))return false;
+    console.log("可以移动，那就开始了");
+    //移动过程
+    var hasMove = false;//标记当前行是否移动过；
+    for(var j=0 ; j<4 ; j++){
+        for(var i=1 ; i<4 ; i++){
+            if(board[i][j]!=0){
+                //判断它左侧的元素,
+                // 这里的逻辑是只需要获取其左侧最近的不等于0的值，
+                // 相等则合并，不相等则移动至该位置+1
+                //根据hasMove可以避免多次合并
+                for(var k=i-1 ; k>=0 ; k--){
+                    console.log(i,j,k);
+                    if(board[k][j]!=0){
+                        if(board[k][j]!=board[i][j]||hasMove ==true){
+
+                            if(k+1<i){
+                                showMoveAnimation(i,j,k+1,j);
+                                board[k+1][j] =board[i][j];
+                                board[i][j] = 0 ;
+
+                            }
+                            k=-1;
+                            hasMove = false;
+                        }else{
+                            showMoveAnimation(i,j,k,j);
+                            board[k][j] += board[i][j];
+                            board[i][j] = 0;
+                            k=-1;
+                            hasMove = true;
+                        }
+                    }else if(k == 0 ){
+                        showMoveAnimation(i,j,k,j);
+                        board[k][j] =board[i][j];
+                        board[i][j] = 0 ;
+                    }
+                }
+            }
+        }
+        hasMove = false;
+
+    }
+    setTimeout("updateBoardView()",200);
+    return true;
+}
+function moveDown(){
+    if(!canMoveDown(board))return false;
+    console.log("可以移动，那就开始了");
+    //移动过程
+    var hasMove = false;//标记当前行是否移动过；
+    for(var j=0 ; j<4 ; j++){
+        for(var i=2 ; i>=0 ; i--){
+            if(board[i][j]!=0){
+                //判断它左侧的元素,
+                // 这里的逻辑是只需要获取其左侧最近的不等于0的值，
+                // 相等则合并，不相等则移动至该位置+1
+                //根据hasMove可以避免多次合并
+                for(var k=i+1 ; k<4 ; k++){
+                    console.log(i,j,k);
+                    if(board[k][j]!=0){
+                        if(board[k][j]!=board[i][j]||hasMove ==true){
+
+                            if(k-1>i){
+                                showMoveAnimation(i,j,k-1,j);
+                                board[k-1][j] =board[i][j];
+                                board[i][j] = 0 ;
+
+                            }
+                            k=4;
+                            hasMove = false;
+                        }else{
+                            showMoveAnimation(i,j,k,j);
+                            board[k][j] += board[i][j];
+                            board[i][j] = 0;
+                            k=4;
+                            hasMove = true;
+                        }
+                    }else if(k == 3 ){
+                        showMoveAnimation(i,j,k,j);
+                        board[k][j] =board[i][j];
+                        board[i][j] = 0 ;
+                    }
+                }
+            }
+        }
+        hasMove = false;
+
+    }
+    setTimeout("updateBoardView()",200);
+    return true;
+}
+
---
+++ 2048/script/support.js
@@ -0,0 +1,122 @@
+/**
+ * Created by lwx on 2018/3/2.
+ */
+
+
+
+
+function getPosTop(i,j){
+    return 20+i*120;
+}
+function getPosLeft(i,j){
+    return 20+j*120;
+}
+function getNumberBackgroundColor(number){
+    switch (number){
+        case 2:return "#CCFFFF";break;
+        case 4:return "#CCFF66";break;
+        case 8:return "#CCFF00";break;
+        case 16:return "#99FF00";break;
+        case 32:return "#99CC00";break;
+        case 64:return "#999933";break;
+        case 128:return "#996633";break;
+        case 256:return "#993333";break;
+        case 512:return "#CC0000";break;
+        case 1024:return "#003399";break;
+        case 2048:return "#663300";break;
+        case 4096:return "#000096";break;
+        return "#000000"
+    }
+}
+function getNumberColor(number){
+    if(number<=8){
+        return "#333300"
+    }else{
+        return "#FFFFCC"
+    }
+}
+//判断是否还有空位
+function nospace(){
+    for(var i=0 ; i<4 ; i++){
+        for(var j=0 ;j<4 ; j++){
+            if(board[i][j]==0)
+            return false;
+        }
+    }
+    return true;
+}
+//判断能否像左移动
+function canMoveLeft(board){
+    for(var i=0 ; i<4 ; i++){
+        for(var j=1 ; j<4; j++){
+            if(board[i][j]!=0){
+                if(board[i][j-1]==0||board[i][j-1]==board[i][j])
+                return true
+            }
+        }
+    }
+    return false;
+}
+//判断能否右移
+function canMoveRight(board){
+    for(var i=0 ; i<4 ; i++){
+        for(var j=2 ; j>=0; j--){
+            if(board[i][j]!=0){
+                if(board[i][j+1]==0||board[i][j+1]==board[i][j])
+                    return true
+            }
+        }
+    }
+    return false;
+}
+//判断能否上移
+function canMoveUp(board){
+    for(var i=1 ; i<4 ; i++){
+        for(var j=0 ; j<4; j++){
+            if(board[i][j]!=0){
+                if(board[i-1][j]==0||board[i-1][j]==board[i][j])
+                    return true
+            }
+        }
+    }
+    return false;
+}
+//判断能否下移
+function canMoveDown(board){
+    for(var i=2 ; i>=0 ; i--){
+        for(var j=0 ; j<4; j++){
+            if(board[i][j]!=0){
+                if(board[i+1][j]==0||board[i+1][j]==board[i][j])
+                    return true
+            }
+        }
+    }
+    return false;
+}
+//判断是否还能移动
+function nomove(board){
+    if(canMoveDown(board)||canMoveLeft(board)||canMoveRight(board)||canMoveUp(board))
+        return false;
+    return true;
+}
+
+
+function showNumberWithAnimation(i,j,number){
+    var numberCell = $('#number-cell'+i+'-'+j);
+    numberCell.css("background-color",getNumberBackgroundColor(number));
+    numberCell.css("color",getNumberColor(number));
+    numberCell.text(number);
+    numberCell.animate({
+        width:"100px",
+        height:"100px",
+        top:getPosTop(i,j),
+        left:getPosLeft(i,j)
+    },50);
+}
+function showMoveAnimation(x1,y1,x2,y2){
+    var numberCell = $('#number-cell-'+x1+'-'+y1);
+    numberCell.animate({
+        top:getPosTop(x2,y2),
+        left:getPosLeft(x2,y2)
+    },200);
+}
